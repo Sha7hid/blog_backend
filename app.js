a6d3d1d6d8a29f7dbec8973f899c7e6226c3aac5
sha7hid <shahidshafadshahad@gmail.com>
2023-02-01 21:03:52 +0530

    updt

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ const connectDB = require('./config/db');
 const mongo_uri =`mongodb+srv://shahid:arthur540913@cluster2.ggcnvuy.mongodb.net/Blog`
 
 connectDB();
-const port = 0.0.0.0:$PORT;
 // Routes
 app.use('/', require('./routes/index'));
-app.listen(port);
+app.listen(0.0.0.0:$PORT);
