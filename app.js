70a67a75ffe83c9334c8b3373946bb7820288c0f
sha7hid <shahidshafadshahad@gmail.com>
2023-02-01 20:36:22 +0530

    updt

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+
 
 
 const app = express();
 app.use(bodyParser.json());
 
-const connectDB = require('./config/db');
+const connectDB = require('./db');
 
-// Load Config
-dotenv.config({path: './config/config.env'});
+const mongo_uri =`mongodb+srv://shahid:arthur540913@cluster2.ggcnvuy.mongodb.net/Blog`
 
 connectDB();
 const port = 3000;
