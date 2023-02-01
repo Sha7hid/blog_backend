476924133a27f82f64aca6c4d5f6b0545c56eb50
sha7hid <shahidshafadshahad@gmail.com>
2023-02-01 20:38:10 +0530

    updt

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-const connectDB = require('./db');
+const connectDB = require('./config/db');
 
 const mongo_uri =`mongodb+srv://shahid:arthur540913@cluster2.ggcnvuy.mongodb.net/Blog`
 
